0dbb160e9a0cc98c291bc599f17d036704a1fd75
Michael Smyers <msmyers@users.noreply.github.com>
2022-07-30 12:21:26 -0700

    initial outline

---
+++ deploy/01_deploy_Example_Typescript.ts
@@ -0,0 +1,19 @@
+// import {HardhatRuntimeEnvironment} from 'hardhat/types/runtime';
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from "hardhat/types/runtime";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const {deployments, getNamedAccounts} = hre;
+    const {deploy} = deployments;
+
+    const {deployer, tokenOwner} = await getNamedAccounts();
+
+    await deploy('Token', {
+        from: deployer,
+        args: [tokenOwner],
+        log: true,
+        deterministicDeployment: true,
+    });
+};
+export default func;
+func.tags = ['Token'];
